262dea92a9ca72b694e321bcc6a5c14173ea9110
René Kooi <renee@kooi.me>
2016-09-04 16:18:14 +0200

    pass unknown cli parameters to build options

--- src/cli.js
+++ src/cli.js
@@ -2,10 +2,14 @@ import 'loud-rejection/register';
 import { createReadStream, unlink } from 'fs';
 import * as path from 'path';
 import { tmpName } from 'tmp';
+import minimist from 'minimist';
 import pify from 'pify';
 import build from './build';
 
-const [entry, output] = process.argv.slice(2);
+const opts = minimist(process.argv.slice(2));
+const args = opts._;
+delete opts._;
+const [entry, output] = args;
 
 const pifyStream = stream => new Promise((resolve, reject) => {
   stream.on('end', resolve).on('error', reject);
@@ -13,12 +17,14 @@ const pifyStream = stream => new Promise((resolve, reject) => {
 
 if (output) {
   build({
+    ...opts,
     entry: path.resolve(entry),
     output,
   });
 } else {
   pify(tmpName)().then(name =>
     build({
+      ...opts,
       entry: path.resolve(entry),
       output: `${name}.js`,
     }).then(() => pifyStream(
