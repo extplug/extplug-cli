3d4de27d63678c84710a28a37098fa9c47e3659b
René Kooi <renee@kooi.me>
2016-09-04 16:33:15 +0200

    move build/watch functions from cli into api module

--- src/cli.js
+++ src/cli.js
@@ -1,36 +1,14 @@
 import 'loud-rejection/register';
-import { createReadStream, unlink } from 'fs';
-import * as path from 'path';
-import { tmpName } from 'tmp';
 import minimist from 'minimist';
-import pify from 'pify';
-import build from './build';
+import { build } from './';
 
 const opts = minimist(process.argv.slice(2));
 const args = opts._;
 delete opts._;
 const [entry, output] = args;
 
-const pifyStream = stream => new Promise((resolve, reject) => {
-  stream.on('end', resolve).on('error', reject);
+build({
+  ...opts,
+  entry,
+  output,
 });
-
-if (output) {
-  build({
-    ...opts,
-    entry: path.resolve(entry),
-    output,
-  });
-} else {
-  pify(tmpName)().then(name =>
-    build({
-      ...opts,
-      entry: path.resolve(entry),
-      output: `${name}.js`,
-    }).then(() => pifyStream(
-      createReadStream(`${name}.js`).pipe(process.stdout)
-    )).then(() =>
-      pify(unlink)(`${name}.js`)
-    )
-  );
-}
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import { createReadStream, unlink } from 'fs';
+import * as path from 'path';
+import pify from 'pify';
+import { tmpName } from 'tmp';
+import makeBundle from './build';
+
+const pifyStream = stream => new Promise((resolve, reject) => {
+  stream.on('end', resolve).on('error', reject);
+});
+
+export function build({ entry, output, ...opts }) {
+  if (output) {
+    return makeBundle({
+      ...opts,
+      entry: path.resolve(entry),
+      output,
+    });
+  }
+
+  return pify(tmpName)().then(name =>
+    makeBundle({
+      ...opts,
+      entry: path.resolve(entry),
+      output: `${name}.js`,
+    }).then(() => pifyStream(
+      createReadStream(`${name}.js`).pipe(process.stdout)
+    )).then(() =>
+      pify(unlink)(`${name}.js`)
+    )
+  );
+}
+
+export function watch(opts) {
+  return build({
+    ...opts,
+    watch: true,
+  });
+}
